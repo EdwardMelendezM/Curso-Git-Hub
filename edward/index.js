da46ed1cd3a12899e80a1c7547789c33689b095f
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-02-10 13:43:12 -0500

    carpeta Edward

---
+++ edward/index.js
@@ -0,0 +1,19 @@
+'use strict';
+var express = require('express')
+const app = express();
+
+//Escuchamos nuestro servidor creado
+app.listen(3001, () => {
+  console.log('listening on 3001')
+})
+
+
+// Primero npm init = iniciar el proyecto -> package.json
+
+// Segundo crear index.js
+
+// Tercero Instalar todas las librerias que contiene nuestro package.json  (npm install)
+
+// Cuarto Instalar express (npm install express)
+
+// Finaliza
